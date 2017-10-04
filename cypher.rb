fe31f33ba8efd376d4be95df9bccb04485ecd357
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-10-04 11:47:54 -0500

    Indentation fix

--- cypher.rb
+++ cypher.rb
@@ -1,29 +1,28 @@
-def ceasar_cypher(sentence, cypher) 
+def ceasar_cypher(sentence, cypher)
   encrypted = []
-  sentence.split("").each do |letter|
+  sentence.split('').each do |letter|
     letter_number = letter.ord
     if /\w/.match(letter)
-     if /[A-Z]/.match(letter)
-      if letter.ord + cypher > 90
-        encrypted.push((((letter_number + cypher) - 90) + 64).chr)
+      if /[A-Z]/.match(letter)
+        if letter.ord + cypher > 90
+          encrypted.push((((letter_number + cypher) - 90) + 64).chr)
+        else
+          encrypted.push((letter_number + cypher).chr)
+        end
       else
-        encrypted.push((letter_number + cypher).chr)
-      end
+        if letter.ord + cypher > 122
+          encrypted.push((((letter_number + cypher) - 122) + 96).chr)
+        else
+          encrypted.push((letter_number + cypher).chr)
+        end
+     end
     else
-      if letter.ord + cypher > 122
-        encrypted.push((((letter_number + cypher) - 122) + 96).chr)
-      else
-        encrypted.push((letter_number + cypher).chr)
-      end
-    end
-  else
-    encrypted.push(letter)
+      encrypted.push(letter)
   end
+  end
+  puts encrypted.join
 end
-puts encrypted.join()
-end 
 
 # a-z = [96..122]
 # A-Z = [65..90]
-ceasar_cypher("My name is Tate!!", 13)
-
+ceasar_cypher('I pledge aliegence to the flag', 10)
